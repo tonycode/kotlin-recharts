235bfc36eec95f725d2b6cf888fe9b01e9fe4409
Anton Vasilev <6095724+tonycode@users.noreply.github.com>
2023-09-16 02:49:33 +0300

    refactor: Extract SimpleLineChart

--- demo/src/jsMain/kotlin/dev/tonycode/kotlin_wrappers/kotlin_recharts_demo/ui/App.kt
+++ demo/src/jsMain/kotlin/dev/tonycode/kotlin_wrappers/kotlin_recharts_demo/ui/App.kt
@@ -5,7 +5,9 @@ import dev.tonycode.kotlin_wrappers.kotlin_recharts_demo.ui.screens.DemoScreen
 import emotion.react.css
 import react.FC
 import react.Props
+import react.dom.html.ReactHTML.b
 import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.p
 import web.cssom.Position
 import web.cssom.pct
 import web.cssom.px
@@ -13,6 +15,18 @@ import web.cssom.px
 
 val App = FC<Props> {
 
+    // header
+    p {
+        css {
+            marginTop = 32.px
+            marginLeft = 24.px
+        }
+
+        b { +"kotlin-recharts" }
+        +" - Examples"
+    }
+
+    // content
     DemoScreen()
 
     // footer
--- demo/src/jsMain/kotlin/dev/tonycode/kotlin_wrappers/kotlin_recharts_demo/ui/screens/DemoScreen.kt
+++ demo/src/jsMain/kotlin/dev/tonycode/kotlin_wrappers/kotlin_recharts_demo/ui/screens/DemoScreen.kt
@@ -1,48 +1,19 @@
 package dev.tonycode.kotlin_wrappers.kotlin_recharts_demo.ui.screens
 
-import dev.tonycode.kotlin_wrappers.kotlin_recharts_demo.repository.stubMeasurements
+import dev.tonycode.kotlin_wrappers.kotlin_recharts_demo.ui.components.SimpleLineChart
+import emotion.react.css
 import react.FC
 import react.Props
-import recharts.CartesianGrid
-import recharts.Legend
-import recharts.Line
-import recharts.LineChart
-import recharts.ResponsiveContainer
-import recharts.Tooltip
-import recharts.XAxis
-import recharts.YAxis
-import recharts.activeDot
-import recharts.strokeDash
-import web.cssom.pct
+import react.dom.html.ReactHTML.div
+import web.cssom.px
 
 
 val DemoScreen = FC<Props> {
 
-    ResponsiveContainer {
-        width = 100.pct
-        height = 600
+    div {
+        css { marginTop = 24.px }
 
-        LineChart {
-            data = stubMeasurements.toTypedArray()
-
-            CartesianGrid {
-                strokeDash(3, 3)
-            }
-            XAxis {
-                dataKey = "day"
-            }
-            YAxis()
-
-            Tooltip()
-            Legend()
-
-            Line {
-                type = "monotone"
-                dataKey = "value"
-                stroke = "#8884d8"
-                activeDot(8)
-            }
-        }
+        SimpleLineChart()
     }
 
 }
